1ec382ad89d10e007b852c03f57f36844a973c83
Hlbleasdale <55389540+Hlbleasdale@users.noreply.github.com>
2019-10-31 16:19:57 +0000

    Finalised documentation

--- model.py
+++ model.py
@@ -1,8 +1,14 @@
 # Import appropriate libraries 
+# Import matplotlib for plotting
 import matplotlib 
+# Ask matplotlib to use 'Tkagg' - this uses a different backend for graphics,
+# this must be done before importing matplotlib.pyplot or it will have no effect
 matplotlib.use('TkAgg')
-import tkinter 
-import matplotlib.animation 
+# Import Tk interface - a GUI toolkit
+import tkinter
+# Import matplotlib.animation to enable animations
+import matplotlib.animation
+# Import matplotlib.pyplot to aid plotting
 import matplotlib.pyplot 
 # Import the 'Agent' class which contains functions for the agents
 import agentframework 
@@ -29,7 +35,7 @@ with open("C:/Users/hopeb/OneDrive/Documents/Fourth Year 201920/ENVS802 LEEDS Pr
         for value in row:
             # Use the append function to add values to the rows 
             environment_row.append(int(value))
-        # Append the rowlist to the environment list
+        # Append the environment_row to the environment list
         environment.append(environment_row)
       
 # Set up an empty list for the agents variable
@@ -83,27 +89,38 @@ def update(frame_number):
     matplotlib.pyplot.ylim(0,300)
     # Set the title
     matplotlib.pyplot.title("Agent Based Model of Sheep in an Environment", fontsize='x-large')
+    # Set the axis labels to 'Environment'
     matplotlib.pyplot.ylabel("Environment")
     matplotlib.pyplot.xlabel("Environment")
         
-    # Once all the sheep have eaten enough, the model stops
+    # Create a stopping function, where when all the sheep have eaten enough, 
+    # the model stops. 
+    # First create an empty variable, total, which will store the number of
+    # agents that have eaten enough
     total = 0
-    print(total)
+    # Create a for-loop, which calls the eaten_enough function and tests whether
+    # the agents are full
     for i in range(num_of_agents):
         agent_full = agents[i].eaten_enough() 
         if agent_full == 1:
+            # When agents are full, add 1 to the total
             total += 1
+            # Test this works by printing
             #print("Sheep", agents[i], "has eaten enough, my store is: {}".format(agents[i].store))
             print("Amount of sheep that have eaten enough:", total)
-            
+    # Once total reaches the number of agents (once all of the sheep are full)        
     if total == (num_of_agents):
+        # Stop the model and print 'stopping condition'
         carry_on = False
         print ("Stopping condition")
         
 # Loop the animation until the number of iterations has been met
 # and until the stopping condition is brought in
 def general_function(b = [0]):
+    # Set up an empty variable, a
     a = 0
+    # continue running the model whilst the number of iterations hasn't been
+    # reached and carry on is still set to True
     global carry_on 
     while (a < num_of_iterations) & (carry_on) :
         yield a			
@@ -130,20 +147,26 @@ for file_name in os.listdir(png_dir):
         file_path = os.path.join(png_dir, file_name)
         images.append(imageio.imread(file_path))
 # Set a location and filename for it to save to
-imageio.mimsave('C:/Users/hopeb/OneDrive/Documents/Fourth Year 201920/ENVS802 LEEDS Programming for Social Scientists/Leeds-Geog-5995/sheepmovie.gif', images, fps=55)
+imageio.mimsave('C:/Users/hopeb/OneDrive/Documents/Fourth Year 201920/ENVS802 LEEDS Programming for Social Scientists/Leeds-Geog-5995/FinalModel.gif', images, duration=10, fps=55)
 
-
+# When the code is run, python creates a window 'root' where the animation is displayed
 root = tkinter.Tk()
+# Set the title of the animation
 root.wm_title("Agent Based Model of Sheep in an Environment")
+# Set a canvas for the animation to be plotted onto
 canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
 canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+# Create a menu bar for the window 
 menu_bar = tkinter.Menu(root)
 root.config(menu=menu_bar)
 model_menu = tkinter.Menu(menu_bar)
 menu_bar.add_cascade(label="Model", menu=model_menu)
+# Create commands which call functions - run and quit
+# This allows the user to click run - to run model
 model_menu.add_command(label="Run model", command=run)
+# And allows the user to click quit - to quit model
 model_menu.add_command(label="Quit model", command=quit)
 
+# The following line of code must go at the end - this runs the animation
 tkinter.mainloop()  
-
 
